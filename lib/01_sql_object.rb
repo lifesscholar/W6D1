66f11858f936681e91959a5e7a40e656ba4a1169
lifesscholar <n_granite@hotmail.com>
2019-12-30 17:29:45 -0500

    classwork done on W6D1

---
+++ lib/01_sql_object.rb
@@ -0,0 +1,102 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    return @columns if @columns
+    columns = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+    columns.map!(&:to_sym)
+    @columns = columns
+  end
+
+  def self.finalize!
+    self.columns.each do |name|
+
+      define_method(name) do 
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+
+    end
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name || self.name.underscore.pluralize
+  end
+
+  def self.all
+    results = DBConnection.execute(<<-SQL)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+    SQL
+
+    parse_all(results)
+  end
+
+  def self.parse_all(results)
+    results.map do |instance|
+      self.new(instance)
+    end
+  end
+
+  def self.find(id)
+    result = DBConnection.execute(<<-SQL, id)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+      WHERE
+        #{table_name}.id = ?
+    SQL
+    parse_all(result).first
+  end
+
+  def initialize(params = {})
+    params.each do |name, value|
+      name = name.to_sym
+      if self.class.columns.include?(name)
+        self.send("#{name}=", value)
+      else
+        raise "unknown attribute '#{name}'"
+      end
+    end
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    self.class.columns.map { |instance| self.send(instance) }
+  end
+
+  def insert
+    columns = self.classs.columns.drop(1)
+    col_names = columns.map(&:to_s).join(",")
+    question_marks
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
